76c7c2e28b0e2c0cafde463d5da93769f752d82f
Samuel Dobsa <samuel.dobsa@amcef.sk>
2024-12-20 21:24:08 +0100

    Creation of versioning according to semantic version

--- src/main/java/com/jabub/SemanticVersionsComparator.java
+++ src/main/java/com/jabub/SemanticVersionsComparator.java
@@ -5,9 +5,6 @@ import java.util.Comparator;
 import java.util.List;
 
 import static com.jabub.Utils.extractVersion;
-import static java.lang.Math.max;
-import static java.util.Arrays.deepToString;
-import static java.util.Arrays.stream;
 
 public class SemanticVersionsComparator implements Comparator<Path> {
 
@@ -21,7 +18,7 @@ public class SemanticVersionsComparator implements Comparator<Path> {
         return compereVersions(version1, version2);
     }
 
-    boolean isHigher (Path script, String version) {
+    boolean isHigher(Path script, String version) {
         return this.compare(script, Path.of(version + "_")) > 0;
     }
 
@@ -32,9 +29,9 @@ public class SemanticVersionsComparator implements Comparator<Path> {
         int[] v1Main = parseMainParts(v1Parts[0]);
         int[] v2Main = parseMainParts(v2Parts[0]);
 
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             int result = Integer.compare(v1Main[i], v2Main[i]);
-            if (result != 0){
+            if (result != 0) {
                 return result;
             }
         }
@@ -48,7 +45,7 @@ public class SemanticVersionsComparator implements Comparator<Path> {
     private int[] parseMainParts(String mainPart) {
         String[] parts = mainPart.split("\\.");
         int[] parsed = new int[3];
-        for(int i = 0; i < parts.length && i < 3; i++){
+        for (int i = 0; i < parts.length && i < 3; i++) {
             parsed[i] = Integer.parseInt(parts[i]);
         }
         return parsed;
@@ -71,48 +68,16 @@ public class SemanticVersionsComparator implements Comparator<Path> {
         if (index1 == -1 && index2 == -1) {
             return pre1.compareTo(pre2);
         }
-        if (index1 == -1){
+        if (index1 == -1) {
             return 1;
         }
-        if (index2 == -1){
+        if (index2 == -1) {
             return -1;
         }
         return Integer.compare(index1, index2);
     }
-
-
-
-//    @Override
-//    public int compare(Path o1, Path o2) {
-//        String version1 = extractVersion(o1);
-//        String version2 = extractVersion(o2);
-//
-//        List<Integer> version1Components = stream(version1.split("\\."))
-//                .map(Integer::parseInt)
-//                .toList();
-//        List<Integer> version2Components = stream(version2.split("\\."))
-//                .map(Integer::parseInt)
-//                .toList();
-//
-//        int maxLength = max(version1Components.size(), version2Components.size());
-//
-//        for (int i = 0; i < maxLength; i++) {
-//            int v1Component = i < version1Components.size() ? version1Components.get(i) : 0;
-//            int v2Component = i < version2Components.size() ? version2Components.get(i) : 0;
-//
-//            if (v1Component > v2Component) {
-//                return 1;
-//            } else if (v1Component < v2Component) {
-//                return -1;
-//            }
-//        }
-//        return 0;
-//    }
-//
-//    boolean isHigher(Path script, String version) {
-//        return this.compare(script, Path.of(version + "_")) > 0;
-//    }
 }
+
 
 
 
